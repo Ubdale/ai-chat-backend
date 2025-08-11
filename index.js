5b95e0be3c983ce33cae0ede738d2718bbef7e5b
Muhammad Ubdale <99432158+Ubdale@users.noreply.github.com>
2025-08-10 23:54:46 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+const {GoogleGenerativeAI} = require('@google/generative-ai');
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+async function generateResponse(prompt) {
+
+  const model =  await genAI.getGenerativeModel({
+     model: "gemini-2.5-flash",
+  })
+  const data = await model.generateContent(prompt)
+  return data;
+  }
+
+app.post('', (req,res)=>{
+  
+  let message = req.body.message;
+  generateResponse(message).then((data) => {
+    res.send({user: 'Bot', message: data.response.candidates[0].content.parts[0].text
+});
+  })
+  // res.send({data: 'Hello from the backend!'});
+});
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
