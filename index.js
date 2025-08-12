722b2f52b70208f100f90d340ea4583c1566abd8
Muhammad Ubdale <99432158+Ubdale@users.noreply.github.com>
2025-08-12 10:38:09 -0700

    again live

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 // ----- CORS Configuration -----
 const allowedOrigins = [
   'https://ai-chat-frontend-beryl.vercel.app',
