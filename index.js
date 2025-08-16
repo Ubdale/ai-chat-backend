f5a67ebc142522f548bf0f4ac1b934d163d3e455
Muhammad Ubdale <99432158+Ubdale@users.noreply.github.com>
2025-08-16 09:33:32 -0700

    cc

--- index.js
+++ index.js
@@ -70,7 +70,7 @@
       }
     }
 
-    app.post('/chat', async (req, res) => {
+    app.post('chat', async (req, res) => {
       try {
         const { message } = req.body;
         if (!message) {
